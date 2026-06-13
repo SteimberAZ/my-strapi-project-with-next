dca1e1d6652ae04fa5d3133cba43b28e9f524c07
Steimber <randyarteaga1519@gmail.com>
2026-06-13 13:17:20 -0500

    feat: add SignupForm component with integration for registerUserAction and form state management

--- frontend/components/sigup-form.tsx
+++ frontend/components/sigup-form.tsx
@@ -101,7 +101,7 @@ export function SignupForm() {
         </Card>
         <div className={styles.prompt}>
           Have an account?
-          <Link className={styles.link} href="/signin">
+          <Link className={styles.link} href="/singin">
             Sign In
           </Link>
         </div>
